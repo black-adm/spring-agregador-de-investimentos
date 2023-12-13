0ada11a0fa877b659ce6f244a513a7db21364027
black-adm <mattheuscontato17@gmail.com>
2023-12-13 07:29:04 -0300

    feat: create client controller

--- src/main/java/com/blackadm/investment/aggregator/controller/ClientController.java
+++ src/main/java/com/blackadm/investment/aggregator/controller/ClientController.java
@@ -1,2 +1,27 @@
-package com.blackadm.investment.aggregator.controller;public class ClientController {
+package com.blackadm.investment.aggregator.controller;
+
+import com.blackadm.investment.aggregator.dto.CreateClientDto;
+import com.blackadm.investment.aggregator.entity.Client;
+import com.blackadm.investment.aggregator.service.ClientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/v1/clients")
+public class ClientController {
+
+    private final ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Client> createClient(@RequestBody CreateClientDto createClientDto) {
+        var clientId = clientService.createClient(createClientDto);
+
+        return ResponseEntity.created(URI.create("/v1/clients/" + clientId.toString())).build();
+    }
 }
